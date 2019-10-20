da29caad5c54dd350b9a23db971c86c8242dc14c
Yahya ElGawady <yahya.elgawady@gmail.com>
2019-10-20 15:12:17 -0500

    Setup Robot team files from examples

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mechanum.java
@@ -0,0 +1,51 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+
+@TeleOp(name="Mechanum", group="Linear Opmode")
+
+public class Mechanum extends LinearOpMode {
+
+
+    private ElapsedTime runtime = new ElapsedTime();
+    private DcMotor frontLeft = null;
+    private DcMotor frontRight = null;
+    private DcMotor backLeft = null;
+    private DcMotor backRight = null;
+
+    @Override
+    public void runOpMode() {
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        frontLeft = hardwareMap.get(DcMotor.class, "");
+        frontRight = hardwareMap.get(DcMotor.class, "right_drive");
+        backLeft = hardwareMap.get (DcMotor.class, "idky");
+        backRight = hardwareMap.get(DcMotor.class, "idket");
+
+
+        waitForStart();
+        runtime.reset();
+        while (opModeIsActive()) {
+            double r = Math.hypot(gamepad1.left_stick_x, gamepad1.left_stick_y);
+            double robotAngle = Math.atan2(gamepad1.left_stick_y, gamepad1.left_stick_x) - Math.PI / 4;
+            double rightX = gamepad1.right_stick_x;
+            final double v1 = r * Math.cos(robotAngle) + rightX;
+            final double v2 = r * Math.sin(robotAngle) - rightX;
+            final double v3 = r * Math.sin(robotAngle) + rightX;
+            final double v4 = r * Math.cos(robotAngle) - rightX;
+
+            frontLeft.setPower(v1);
+            frontRight.setPower(v2);
+            backLeft.setPower(v3);
+            backRight.setPower(v4);
+
+        }
+    }
+}
