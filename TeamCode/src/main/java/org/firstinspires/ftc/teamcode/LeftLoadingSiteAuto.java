1ebc9fae2c06beb930f07a87a1292d29feca9640
Yahya ElGawady <yahya.elgawady@gmail.com>
2019-11-24 13:33:18 -0600

    Salam Alakum

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LeftLoadingSiteAuto.java
@@ -0,0 +1,215 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+
+
+
+@Autonomous(name="LeftLoadingSite", group="Linear Opmode")
+
+public class LeftLoadingSiteAuto extends LinearOpMode {
+
+    private ElapsedTime runtime = new ElapsedTime();
+    private DcMotor frontLeft = null;
+    private DcMotor frontRight = null;
+
+    private DcMotor backLeft = null;
+    private DcMotor backRight = null;
+    private DcMotor rightIntake = null;
+    private DcMotor leftIntake = null;
+
+
+    static final double     COUNTS_PER_MOTOR_REV    = 537.6 ;    // eg: TETRIX Motor Encoder
+    static final double     DRIVE_GEAR_REDUCTION    = 2 ;     // This is < 1.0 if geared UP
+    static final double     WHEEL_DIAMETER_INCHES   = 4.0 ;     // For figuring circumference
+    static final double     COUNTS_PER_INCH         = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) / (WHEEL_DIAMETER_INCHES * 3.1415);
+
+    @Override
+    public void runOpMode() {
+
+        //hardware mapping
+        leftIntake = hardwareMap.get(DcMotor.class, "Left Intake");
+        rightIntake = hardwareMap.get(DcMotor.class,"right intake");
+        backLeft = hardwareMap.get(DcMotor.class,"back left wheel") ;
+        backRight = hardwareMap.get(DcMotor.class,"back right wheel");
+        frontRight = hardwareMap.get(DcMotor.class,"Front Right wheel");
+        frontLeft = hardwareMap.get(DcMotor.class,"Front Left wheel");
+
+        backLeft.setDirection(DcMotor.Direction.FORWARD);
+        frontLeft.setDirection(DcMotor.Direction.REVERSE);
+        frontRight.setDirection(DcMotor.Direction.REVERSE);
+
+        frontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+
+        // Send telemetry message to signify robot waiting;
+        telemetry.addData("Status", "Resetting Encoders");    //
+        telemetry.update();
+
+        frontLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        frontRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        frontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        frontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        // Send telemetry message to indicate successful Encoder reset
+        telemetry.addData("Path0",  "Starting at %7d :%7d",
+                frontLeft.getCurrentPosition(),
+                frontRight.getCurrentPosition(),
+                backLeft.getCurrentPosition(),
+                backRight.getCurrentPosition());
+        telemetry.update();
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+
+        // Step through each leg of the path,
+        // Note: Reverse movement is obtained by setting a negative distance (not speed)
+        encoderDrive(0.6,  -10,  10,-10,-10, 5.0);  // S1: Forward 47 Inches with 5 Sec timeout
+
+        telemetry.addData("Path", "Complete");
+        telemetry.update();
+    }
+    //    public void MovingStraight(double inches, String Status) {
+//        //name variables
+//        double ENCODERS_PER_INCH=1440;
+//        double FRONTLEFT_CURRENT_POSITION = Math.abs(frontLeft.getCurrentPosition());
+//        double FRONTRIGHT_CURRENT_POSITION =Math.abs(frontRight.getCurrentPosition());
+//        double BACKLEFT_CURRENT_POSITION = Math.abs(backLeft.getCurrentPosition());
+//        double BACKRIGHT_CURRENT_POSITION = Math.abs(backRight.getCurrentPosition());
+//        double FRONTLEFT_TARGET_POSITION = Math.abs(frontLeft.getCurrentPosition())+ inches*ENCODERS_PER_INCH;
+//        double FRONTRIGHT_TARGET_POSITION = Math.abs(frontRight.getCurrentPosition())+ inches*ENCODERS_PER_INCH;
+//        double BACKLEFT_TARGET_POSITION = Math.abs(backLeft.getCurrentPosition())+ inches*ENCODERS_PER_INCH;
+//        double BACKRIGHT_TARGET_POSITION = Math.abs(backRight.getCurrentPosition()) + inches*ENCODERS_PER_INCH;
+//
+//        //create an array for variables
+//        double [] currentPositions= {FRONTLEFT_CURRENT_POSITION, FRONTRIGHT_CURRENT_POSITION, BACKLEFT_CURRENT_POSITION, BACKRIGHT_CURRENT_POSITION};
+//        double [] targetPositions = {FRONTLEFT_TARGET_POSITION, FRONTRIGHT_TARGET_POSITION, BACKLEFT_TARGET_POSITION, BACKRIGHT_TARGET_POSITION};
+//
+//        //Boolean
+//        boolean currentEqualsTarget = false;
+//            while (currentEqualsTarget == false) {
+//                 FRONTLEFT_CURRENT_POSITION = Math.abs(frontLeft.getCurrentPosition());
+//                 FRONTRIGHT_CURRENT_POSITION =Math.abs(frontRight.getCurrentPosition());
+//                 BACKLEFT_CURRENT_POSITION = Math.abs(backLeft.getCurrentPosition());
+//                 BACKRIGHT_CURRENT_POSITION = Math.abs(backRight.getCurrentPosition());;
+//
+//                    for (int i=0;i<=3;i++ ) {
+//                        if (currentPositions[i] >= targetPositions[i]) {
+//                            currentEqualsTarget = true;
+//                        } else if (currentPositions[i] < targetPositions[i]) {
+//                            currentEqualsTarget = false;
+//                            break;
+//                        }
+//                    }
+//
+//                    if(currentEqualsTarget ==  false)
+//                    {
+//                        frontRight.setPower(-1);
+//                        backRight.setPower(-1);
+//                        backLeft.setPower(-1);
+//                        frontLeft.setPower(-1);
+//                        telemetry.addData("Back Left ",BACKLEFT_CURRENT_POSITION);
+//                        telemetry.addData("Front Left ",FRONTLEFT_CURRENT_POSITION);
+//                        telemetry.addData("Back Right ",BACKRIGHT_CURRENT_POSITION);
+//                        telemetry.addData("Front Right ",FRONTRIGHT_CURRENT_POSITION);
+//                        telemetry.update();
+//
+//                    }
+//            }
+//
+//        if (currentEqualsTarget)
+//        {
+//            frontRight.setPower(0);
+//            backRight.setPower(0);
+//            backLeft.setPower(0);
+//            frontLeft.setPower(0);
+//        }
+//    }
+    public void encoderDrive(double speed,
+                             double frontLeftInches, double frontRightInches, double backLeftInches, double backRightInches,
+                             double timeoutS) {
+        int newFrontLeftTarget;
+        int newFrontRightTarget;
+        int newBackLeftTarget;
+        int newBackRightTarget;
+
+        // Ensure that the opmode is still active
+        if (opModeIsActive()) {
+
+            // Determine new target position, and pass to motor controller
+            newFrontLeftTarget = frontLeft.getCurrentPosition() + (int)(frontLeftInches * COUNTS_PER_INCH);
+            newFrontRightTarget = frontRight.getCurrentPosition() + (int)(frontRightInches * COUNTS_PER_INCH);
+            newBackLeftTarget = backLeft.getCurrentPosition() + (int)(backLeftInches * COUNTS_PER_INCH);
+            newBackRightTarget = backRight.getCurrentPosition() + (int)(backRightInches * COUNTS_PER_INCH);
+            frontLeft.setTargetPosition(newFrontLeftTarget);
+            frontRight.setTargetPosition(newFrontRightTarget);
+            backLeft.setTargetPosition(newBackLeftTarget);
+            backRight.setTargetPosition(newBackRightTarget);
+
+            // Turn On RUN_TO_POSITION
+            frontLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            frontRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            backLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            backRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+            // reset the timeout time and start motion.
+            runtime.reset();
+            frontLeft.setPower(Math.abs(speed));
+            frontRight.setPower(Math.abs(speed));
+            backLeft.setPower(Math.abs(speed));
+            backRight.setPower(Math.abs(speed));
+
+            // keep looping while we are still active, and there is time left, and both motors are running.
+            // Note: We use (isBusy() && isBusy()) in the loop test, which means that when EITHER motor hits
+            // its target position, the motion will stop.  This is "safer" in the event that the robot will
+            // always end the motion as soon as possible.
+            // However, if you require that BOTH motors have finished their moves before the robot continues
+            // onto the next step, use (isBusy() || isBusy()) in the loop test.
+            while (opModeIsActive() &&
+                    (runtime.seconds() < timeoutS) &&
+                    (frontLeft.isBusy() && frontRight.isBusy()) && (backLeft.isBusy() && backRight.isBusy())) {
+
+                // Display it for the driver.
+                telemetry.addData("Path1",  "Running to %7d :%7d", newFrontLeftTarget,newBackLeftTarget, newFrontRightTarget, newBackRightTarget);
+                telemetry.addData("Path2",  "Running at %7d :%7d",
+
+                        frontLeft.getCurrentPosition(),
+                        frontRight.getCurrentPosition(),
+                        backLeft.getCurrentPosition(),
+                        backRight.getCurrentPosition());
+                telemetry.update();
+            }
+
+            // Stop all motion;
+
+            frontLeft.setPower(0);
+            frontRight.setPower(0);
+            backLeft.setPower(0);
+            backRight.setPower(0);
+
+            // Turn off RUN_TO_POSITION
+            frontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            frontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            backLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            backRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+            //  sleep(250);   // optional pause after eah move
+        }
+    }
+
+
+}
+
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mechanum.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mechanum.java
@@ -2,50 +2,146 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
+
+
 
 
 @TeleOp(name="Mechanum", group="Linear Opmode")
 
 public class Mechanum extends LinearOpMode {
 
-
+    //define the motors
     private ElapsedTime runtime = new ElapsedTime();
     private DcMotor frontLeft = null;
     private DcMotor frontRight = null;
     private DcMotor backLeft = null;
     private DcMotor backRight = null;
+    private DcMotor rightIntake = null;
+    private DcMotor leftIntake = null;
 
     @Override
     public void runOpMode() {
-        telemetry.addData("Status", "Initialized");
-        telemetry.update();
 
-        frontLeft = hardwareMap.get(DcMotor.class, "");
-        frontRight = hardwareMap.get(DcMotor.class, "right_drive");
-        backLeft = hardwareMap.get (DcMotor.class, "idky");
-        backRight = hardwareMap.get(DcMotor.class, "idket");
+//hardware mapping
+        leftIntake = hardwareMap.get(DcMotor.class, "Left Intake");
+        rightIntake = hardwareMap.get(DcMotor.class,"right intake");
+        backLeft = hardwareMap.get(DcMotor.class,"back left wheel") ;
+        backRight = hardwareMap.get(DcMotor.class,"back right wheel");
+        frontRight = hardwareMap.get(DcMotor.class,"Front Right wheel");
+        frontLeft = hardwareMap.get(DcMotor.class,"Front Left wheel");
 
-
+        backLeft.setDirection(DcMotor.Direction.REVERSE);
+        frontLeft.setDirection(DcMotor.Direction.REVERSE);
+        int STOP = 0;
+        int FORWARD = 1;
+        int BACKWARD = -1;
         waitForStart();
-        runtime.reset();
         while (opModeIsActive()) {
-            double r = Math.hypot(gamepad1.left_stick_x, gamepad1.left_stick_y);
-            double robotAngle = Math.atan2(gamepad1.left_stick_y, gamepad1.left_stick_x) - Math.PI / 4;
-            double rightX = gamepad1.right_stick_x;
-            final double v1 = r * Math.cos(robotAngle) + rightX;
-            final double v2 = r * Math.sin(robotAngle) - rightX;
-            final double v3 = r * Math.sin(robotAngle) + rightX;
-            final double v4 = r * Math.cos(robotAngle) - rightX;
-
-            frontLeft.setPower(v1);
-            frontRight.setPower(v2);
-            backLeft.setPower(v3);
-            backRight.setPower(v4);
-
+            drivetrain(gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.right_stick_x);
+            if (gamepad1.right_bumper)
+            {
+                leftIntake.setPower(FORWARD);
+                rightIntake.setPower(BACKWARD);
+            }
+            else if (gamepad1.left_bumper)
+            {
+                leftIntake.setPower(BACKWARD);
+                rightIntake.setPower(FORWARD);
+            }
+            else
+            {
+                leftIntake.setPower(STOP);
+                rightIntake.setPower(STOP);
+            }
+            if (gamepad2.a)
+            {
+                backLeft.setPower(1);
+            }
+            else if (gamepad2.b)
+            {
+                frontLeft.setPower(BACKWARD);
+            }
+            else if(gamepad2.x)
+            {
+                frontRight.setPower(1);
+            }
+            else if(gamepad2.y)
+            {
+                backRight.setPower(1);
+            }
         }
     }
+
+
+    private void drivetrain(double forward, double right, double turn) {
+
+        forward = checkValue(forward);
+        right = checkValue(right);
+        turn = checkValue(turn);
+        double leftFrontPower = forward - right - turn;
+        double leftBackPower = forward + right - turn;
+        double rightFrontPower = forward + right + turn;
+        double rightBackPower = forward - right + turn;
+        double[] powers = {leftBackPower, leftFrontPower, rightBackPower, rightFrontPower};
+        boolean isItTooBig = false;
+        for (double power : powers) {
+            if (Math.abs(power) > 1) {
+                isItTooBig = true;
+            }
+        }
+        if (isItTooBig) {
+            double greatest = 0;
+            for(double power :powers){
+
+                if(Math.abs(power)>greatest){
+                    greatest=Math.abs(power);
+                }
+
+            }
+            leftBackPower =leftBackPower/greatest;
+            leftFrontPower =leftFrontPower/greatest;
+            rightBackPower =rightBackPower/greatest;
+            rightFrontPower =rightFrontPower/greatest;
+        }
+        boolean stop = true;
+        for (double power : powers)
+        {
+
+            if(Math.abs(power)>.1)
+            {
+                stop = false;
+            }
+        }
+        if (stop)
+        {
+            frontRight.setPower(0);
+            backRight.setPower(0);
+            backLeft.setPower(0);
+            frontLeft.setPower(0);
+        }
+        else {
+            frontRight.setPower(rightFrontPower);
+            backRight.setPower(rightBackPower);
+            backLeft.setPower(leftBackPower);
+            frontLeft.setPower(leftFrontPower);
+        }
+        telemetry.addData("left y-stick", gamepad1.left_stick_y);
+        telemetry.addData("left x-stick", gamepad1.left_stick_x);
+        telemetry.addData("right x-stick", gamepad1.right_stick_x);
+        telemetry.addData("Front right power ", rightFrontPower);
+        telemetry.addData("Front left power ", leftFrontPower);
+        telemetry.addData("Back right power ", rightBackPower);
+        telemetry.addData("Back left power ", leftBackPower);
+        telemetry.update();
+    }
+    private double checkValue(double hardInput) {
+        hardInput = Range.clip(hardInput, -1,1);
+        hardInput = Math.pow(hardInput,3);
+        return hardInput;
+    }
 }
+
