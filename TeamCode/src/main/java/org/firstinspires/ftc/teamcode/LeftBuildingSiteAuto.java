e57e181396b844e8aa7faacbbcf5b60b76acf96e
SarahBee <sbeetner@yahoo.com>
2019-12-11 19:55:33 -0600

    Change Path
    Add arc and 90 degree turn math

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LeftBuildingSiteAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LeftBuildingSiteAuto.java
@@ -107,10 +107,25 @@ public class LeftBuildingSiteAuto extends LinearOpMode {
         left 10 inches with 5 sec timeout?
  */
 
-        telemetry.addData("Initial left Strafe", "Begun");
+        telemetry.addData("move Forward 6 inches", "Begun");
         telemetry.update();
-        encoderDrive(.6, 48.25,-48.25,48.25,48.25,60);
-        telemetry.addData("Initial left Strafe", "Complete");
+        encoderDrive(.6, -6,-6,6, -6,10);
+        telemetry.addData("Move Forward 6 inches", "Complete");
+
+        telemetry.addData("right Strafe", "Begun");
+        telemetry.update();
+        encoderDrive(.6, -10,10,-10,-10,60);
+        telemetry.addData("right Strafe", "Complete");
+
+        telemetry.addData("right 90 degree turn", "Begun");
+        telemetry.update();
+        encoderDrive(.6, -77.782179822,77.782179822,77.782179822,77.782179822,60);
+        telemetry.addData("right 90 degree turn", "Complete");
+
+        telemetry.addData("move Forward 21 inches", "Begun");
+        telemetry.update();
+        encoderDrive(.6, -21,-21,21, -21,10);
+        telemetry.addData("Move Forward 6 inches", "Complete");
 
         telemetry.addData("Lower foundation mover","Start");
         LeftBaseplateShover.setPosition(.7);
@@ -123,10 +138,15 @@ public class LeftBuildingSiteAuto extends LinearOpMode {
 
         }
 
-        telemetry.addData("Strafe right 43 inches", "Begun");
+        telemetry.addData("Arc", "Begun");
         telemetry.update();
-        encoderDrive(.6, -43, 43, -43, -43, 10);
-        telemetry.addData("Strafe Right 43 inches", "Complete");
+        arcTurn(.6, -77.782179822, 26.7035375555, 28.2743338823, 10);
+        telemetry.addData("Arc", "Complete");
+
+        telemetry.addData("move Forward 18 inches", "Begun");
+        telemetry.update();
+        encoderDrive(.6, -18,-18,18, -18,10);
+        telemetry.addData("Move Forward 18 inches", "Complete");
 
         telemetry.addData("Raise foundation mover","Start");
         LeftBaseplateShover.setPosition(0);
@@ -139,10 +159,10 @@ public class LeftBuildingSiteAuto extends LinearOpMode {
 
         }
 
-        telemetry.addData("move Backward 18 inches to the foundation", "Begun");
+        telemetry.addData("move Backward 44.5 inches to park", "Begun");
         telemetry.update();
-        encoderDrive(.6, 18,18,-18, 18,10);
-        telemetry.addData("Move Backward 18 inches to the foundation", "Complete");
+        encoderDrive(.6, 44.5,44.5,-44.5, 44.5,10);
+        telemetry.addData("Move Backward 44.5 inches to park", "Complete");
     }
 
     public void encoderDrive(double speed,
@@ -217,7 +237,67 @@ public class LeftBuildingSiteAuto extends LinearOpMode {
             //  sleep(250);   // optional pause after eah move
         }
     }
+    public void arcTurn (double speed,
+                             double frontLeftInches, double frontRightInches, double backRightInches,
+                             double timeoutS) {
+        int newFrontLeftTarget;
+        int newFrontRightTarget;
+        int newBackLeftTarget;
+        int newBackRightTarget;
 
+        // Ensure that the opmode is still active
+        if (opModeIsActive()) {
+
+            // Determine new target position, and pass to motor controller
+            newFrontLeftTarget = frontLeft.getCurrentPosition() + (int)(frontLeftInches * COUNTS_PER_INCH);
+            newFrontRightTarget = frontRight.getCurrentPosition() + (int)(frontRightInches * COUNTS_PER_INCH);
+            newBackRightTarget = backRight.getCurrentPosition() + (int)(backRightInches * COUNTS_PER_INCH);
+            frontLeft.setTargetPosition(newFrontLeftTarget);
+            frontRight.setTargetPosition(newFrontRightTarget);
+            backRight.setTargetPosition(newBackRightTarget);
+
+            // Turn On RUN_TO_POSITION
+            frontLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            frontRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            backRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+            // reset the timeout time and start motion.
+            runtime.reset();
+            frontLeft.setPower(Math.abs(speed));
+            frontRight.setPower(Math.abs(speed));
+            backRight.setPower(Math.abs(speed));
+
+            while (opModeIsActive() &&
+                    (runtime.seconds() < timeoutS) &&
+                    (frontLeft.isBusy() || frontRight.isBusy()) || backRight.isBusy()) {
+
+                // Display it for the driver.
+                telemetry.addData("Path1",  "Running to %7d :%7d", newFrontLeftTarget, newFrontRightTarget, newBackRightTarget);
+                telemetry.addData("Path2",  "Running at %7d :%7d",
+
+                        frontLeft.getCurrentPosition(),
+                        frontRight.getCurrentPosition(),
+                        backLeft.getCurrentPosition(),
+                        backRight.getCurrentPosition());
+                telemetry.update();
+
+            }
+
+            // Stop all motion;
+
+            frontLeft.setPower(0);
+            frontRight.setPower(0);
+            backLeft.setPower(0);
+            backRight.setPower(0);
+
+            // Turn off RUN_TO_POSITION
+            frontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            frontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            backRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+            //  sleep(250);   // optional pause after eah move
+        }
+    }
 
 }
 
